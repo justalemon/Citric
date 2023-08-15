1e7b5015fa6468937cd0a937eb86983da2e2b987
Hannele Ruiz <justlemoncl@gmail.com>
2023-08-14 23:41:28 -0400

    Allow bans to be infinite

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ function kickPlayer(player: number, reason: string) {
 
 function banPlayer(player: number, reason: string, expires: number) {
     if (GetResourceState("easyadmin") == "started") {
-        TriggerServerEvent("EasyAdmin:addBan", player, reason, expires);
+        TriggerServerEvent("EasyAdmin:addBan", player, reason, expires > 0 ? expires : null);
     } else {
         console.error("There is no resource running for admin management!");
     }
