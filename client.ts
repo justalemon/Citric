546e8dae765d2ccec399cfd7477d8f3ce0743538
Hannele Ruiz <justlemoncl@gmail.com>
2023-08-15 00:14:14 -0400

    Register the client events

--- client.ts
+++ client.ts
@@ -1,1 +1,17 @@
+import { clientEvents } from "./events";
+
 const debug = GetConvarInt("citric_debug", 0) !== 0;
+
+function registerEvents() {
+    for (const event of clientEvents) {
+        on(event, () => {
+            TriggerServerEvent("citric:banEvent");
+        });
+
+        if (debug) {
+            console.log("Registered event " + event)
+        }
+    }
+}
+
+setImmediate(registerEvents);
